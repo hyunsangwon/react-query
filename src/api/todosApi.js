6acb133345a0fb237bfa9cfac69aca7a76c0f28b
hyunsangwon <hyunsangwon93@gmail.com>
2022-09-07 17:05:13 +0900

    feat : add

---
+++ src/api/todosApi.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+const todosApi = axios.create({
+  baseURL: 'http://localhost:3500',
+});
+
+export const getTodos = async () => {
+  const response = await todosApi.get('/todos');
+  return response;
+};
