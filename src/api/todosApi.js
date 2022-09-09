7c898faf85b3ef3e428ae05798f4bcedfa95bd65
hyunsangwon <hyunsangwon93@gmail.com>
2022-09-09 15:55:34 +0900

    feat : post, update, delete 추가

--- src/api/todosApi.js
+++ src/api/todosApi.js
@@ -8,3 +8,15 @@ export const getTodos = async () => {
   const response = await todosApi.get('/todos');
   return response;
 };
+
+export const addTodo = async (todo) => {
+  return await todosApi.post('/todos', todo);
+};
+
+export const updateTodo = async (todo) => {
+  return await todosApi.patch('/todos', todo);
+};
+
+export const deleteTodo = async (todo) => {
+  return await todosApi.delete('/todos', todo);
+};
